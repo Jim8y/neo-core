8ceff33aa8076750d79a9447006084740f8bb49c
Jinghui Liao <jinghui@wayne.edu>
2021-07-27 10:57:47 +0800

    └───neo-core     ├───bin     │   ├───Debug     │   │   └───net5.0     │   │       └───ref     │   └───Release     │       └───net5.0     │           └───ref     ├───Cryptography     │   └───ECC     ├───IO     │   ├───Actors     │   ├───Caching     │   └───Json     ├───obj     │   ├───Debug     │   │   └───net5.0     │   │       └───ref     │   └───Release     │       └───net5.0     │           └───ref     ├───Script     └───Types         └───Collections

---
+++ neo-core/Cryptography/MerkleTreeNode.cs
@@ -0,0 +1,16 @@
+using Neo.Types;
+
+namespace Neo.Cryptography
+{
+    internal class MerkleTreeNode
+    {
+        public UInt256 Hash;
+        public MerkleTreeNode Parent;
+        public MerkleTreeNode LeftChild;
+        public MerkleTreeNode RightChild;
+
+        public bool IsLeaf => LeftChild == null && RightChild == null;
+
+        public bool IsRoot => Parent == null;
+    }
+}
---
+++ neo-core/Utility.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Numerics;
+using System.Text;
+
+namespace Neo.Core
+{
+    internal static class Utility
+    {
+        public static Encoding StrictUTF8 { get; }
+
+        static Utility()
+        {
+            StrictUTF8 = (Encoding)Encoding.UTF8.Clone();
+            StrictUTF8.DecoderFallback = DecoderFallback.ExceptionFallback;
+            StrictUTF8.EncoderFallback = EncoderFallback.ExceptionFallback;
+        }
+
+        public static BigInteger Sqrt(this BigInteger value)
+        {
+            if (value < 0) throw new InvalidOperationException("value can not be negative");
+            if (value.IsZero) return BigInteger.Zero;
+            if (value < 4) return BigInteger.One;
+
+            var z = value;
+            var x = BigInteger.One << (int)(((value - 1).GetBitLength() + 1) >> 1);
+            while (x < z)
+            {
+                z = x;
+                x = (value / x + x) / 2;
+            }
+
+            return z;
+        }
+    }
+}
